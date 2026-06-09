c64224024fa5e40753473b367fb69b9f329ae519
lynkpoRK24 <leonardo.aragao@nave.org.br>
2026-06-09 08:53:26 -0300

    atv concluida,porém só fiz as 3/4

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,4 +7,26 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
   */
+const express = require('express');
+
+const path = require('path');
+
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtividade.html'));
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
+
 
--- src/AtvEnviarJson.js
+++ src/AtvEnviarJson.js
@@ -13,3 +13,43 @@
   * }
   */
 
+const express = require('express');
+
+
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+
+
+app.get('/objeto', (req, res) => {
+  res.json({
+    titulo: "Responda a pergunta abaixo:",
+    conteudo: "Quem é mais redondo, o Kirby ou o Jigglypuff?"
+  });
+});
+app.get('/objeto', (req, res) => {
+  res.send(`
+   <html>
+      <head>
+        <title>Exemplo de página</title>
+      </head>
+      <body>
+        <h1>Você acessou a página de início!</h1>
+        <p>Passe um PIX de 1 real como pedágio :D</p>
+      </body>
+    </html>
+    `);
+});
+app.get("/index1", (req, res) => {
+  res.sendFile(path.join(__dirname, 'indexAtividade.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
