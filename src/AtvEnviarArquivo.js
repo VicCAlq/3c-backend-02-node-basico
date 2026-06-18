4902161492d1378e6b3ef3694e7788d87eb80c49
willyv00 <williane.vitoria@nave.org.br>
2026-06-18 07:55:05 -0300

    feito

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,4 +7,11 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
   */
+const express = require('express')
+const app = express()
 
+app.get('/index',(req,res)=>{
+   res.sendFile(path.join(__dirname, 'indexAtividade.html'))
+})
+
+app.listen(3000, () => {})
