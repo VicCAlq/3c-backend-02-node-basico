0c4d1c0a75f1782e41024c989833198a2d6e86ec
Elyson <elyson.franca@nave.org.br>
2026-06-25 23:20:38 -0300

    ..

--- src/AtvEnviarJson.js
+++ src/AtvEnviarJson.js
@@ -13,3 +13,16 @@
   * }
   */
 
+const express = require('express');
+const app = express();
+
+app.get('/objeto', (req, res) => {
+  res.json({
+    titulo: "Responda a pergunta abaixo:",
+    pergunta: "Quem é mais redondo, o Kirby ou o Jigglypuff?",
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
