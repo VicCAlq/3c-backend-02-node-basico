b921a68fd77c9d64165feb3cce14bcd2d6a6d131
Abraao2025 <abraao.domingos@nave.org.br>
2026-07-02 08:58:46 -0300

    Update AtvEnviarArquivo.js

--- src/AtvEnviarArquivo.js
+++ src/AtvEnviarArquivo.js
@@ -7,4 +7,11 @@
   * Ao acessar esta rota, deve ser enviado o arquivo 
   * "indexAtividade.js" de dentro da pasta src:
   */
+const path = require('path');
 
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src' ,'indexAtividade.js'))
+  res.sendFile(path.join(__dirname, 'src' ,'indexAtividade.js'));
+})
+
+app.listen(3000, ()=>{})
